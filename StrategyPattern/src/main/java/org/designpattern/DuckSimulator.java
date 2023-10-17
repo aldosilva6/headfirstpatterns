f76dc3914c3f4d041f5942aad1746233e205e5c0
Aldo Inácio da Silva <aldosilva6@gmail.com>
2023-10-17 09:05:28 -0300

    Added other examples

--- StrategyPattern/src/main/java/org/designpattern/DuckSimulator.java
+++ StrategyPattern/src/main/java/org/designpattern/DuckSimulator.java
@@ -10,14 +10,14 @@ public class DuckSimulator
 {
     public static void main( String[] args )
     {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+
         Duck model = new ModelDuck();
         model.performFly();
         model.performQuack();
         model.setFlyBehavior(new FlyRocketPowerd());
         model.performFly();
-
-        /*Duck mallard = new MallardDuck();
-        mallard.performQuack();
-        mallard.performFly();*/
     }
 }
